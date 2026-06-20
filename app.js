86de3e24610857d8bed19e357ea1ba161df47846
sahil <sahilsheikh124567@gmail.com>
2026-06-20 18:30:35 +0530

    added cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 const port=3000;
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://pixel1045.netlify.app",
   credentials: true
 }));
 
